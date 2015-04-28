8a48c3c13e625d5560219dff3884940a48cd41ca
Rakesh Singh <kumar.rakesh@gmail.com>
2015-04-27 22:45:40 -0700

    updated url pattern matching logic

--- trendin/urlanalyzer.py
+++ trendin/urlanalyzer.py
@@ -1,19 +1,18 @@
 import csv
 import re
+import time
+from urllib import request
 
 
 def read_data():
+    datafile = '/tmp/' + 'data_' + time.strftime("%Y-%m-%d") + ".tsv"
     pattern_url = re.compile(
         'http[s]?://(?:[a-zA-Z]|[0-9]|[$-_@.&+]|[!*\(\),]|(?:%[0-9a-fA-F][0-9a-fA-F]))+')
-    with open('data.tsv') as tsvfile:
-        tsvreader = csv.reader(tsvfile,	delimiter='\t')
-        for row in tsvreader:
-            # if pattern_url.match(row['']):
-            # print('\t'.join(row))
-            if len(row) < 4:
-                # print(row[0], " - ", row[4])
-                # if pattern_url.match(row[4]):
-                print('\t'.join(row))
+    with open(datafile) as csvfile:
+        csvreader = csv.reader(csvfile, delimiter='\t', quotechar='|')
+        for row in csvreader:
+            if pattern_url.search(row[4]):
+                print('\t'.join(pattern_url.findall(row[4])))
 
 
 def get_urls(text):
@@ -23,17 +22,25 @@ def get_urls(text):
     return None
 
 
-def get_url_details():
+def get_url_details(url):
     '''
     this function returns details of an url
     url_subject: a subject for the url
     url_type: image, video
     '''
+    try:
+        fp = request.urlopen(url)
+        print(fp.geturl())
+    except URLError as error:
+        print("Error occurred: {0}".format(error))
+    except exception:
+        raise exception
     return None
 
 
 def main():
-    read_data()
+    # read_data()
+    get_url_details('http://t.co/vYnYlsFU6U')
 
 
 if __name__ == '__main__':
